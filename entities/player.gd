ec1518314d2e74bf314a1bf1a6bbffefdb586c8e
Martin Felis <martin@fysx.org>
2025-01-25 10:39:43 +0100

    More responsive controls by mapping velocity onto direction.

--- entities/player.gd
+++ entities/player.gd
@@ -1,14 +1,25 @@
 extends RigidBody3D
 
+# signals
 
-const SPEED = 10.0
-const ACCEL = 34.0
-const JUMP_VELOCITY = 4.5
-const BOUNCE_ACCEL_TIMEOUT = 0.2
+# enums
 
+# constants
+const SPEED:float = 10.0
+const ACCEL:float = 34.0
+const JUMP_VELOCITY:float = 4.5
+const BOUNCE_ACCEL_TIMEOUT:float = 0.2
+
+# @export variables
+@export var PROJECT_VELOCITY_ON_DIRECTION:bool = true
+
+# public variables
+
+# private variables
 var _velocity:Vector3 = Vector3.ZERO
 var _bounce_timeout:float = 0
 
+# @onready variables
 @onready var ground: Node3D = %Ground
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
@@ -41,6 +52,11 @@ func _physics_process(delta: float) -> void:
 	if current_speed > SPEED:
 		_velocity = _velocity.normalized() * previous_speed
 	
+	if direction and PROJECT_VELOCITY_ON_DIRECTION:
+		_velocity = direction.normalized() * _velocity.length()
+#		var direction_correction = min(1.0, delta / VELOCITY_DIRECTION_CORRECTION_PER_SECOND)
+#		_velocity = lerp(_velocity, direction.normalized() * _velocity.length(), direction_correction)
+	
 func _on_body_entered(body: Node) -> void:
 	if body == ground:
 		pass
@@ -48,10 +64,9 @@ func _on_body_entered(body: Node) -> void:
 	var other_rigid_body:RigidBody3D = body as RigidBody3D
 	if other_rigid_body:
 		var vector_to_body:Vector3 = body.global_position - global_position
+		vector_to_body.y = 0
 		DebugDraw3D.draw_arrow(Vector3.UP, body.global_position + Vector3.UP, Color.RED, 0.4, true, 2.0)
-		other_rigid_body.apply_impulse((vector_to_body).normalized() * 5)
-
-
+		other_rigid_body.apply_impulse((vector_to_body).normalized() * 2)
 
 	_velocity = Vector3.ZERO
 	#_bounce_timeout = BOUNCE_ACCEL_TIMEOUT
